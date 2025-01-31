77dc8d2c8e1fff8a5cbcda43f42676b5f15a284a
Ting <baiyuting652@gmail.com>
2025-02-01 10:37:18 +1100

    complete translation feature

--- src/worker.js
+++ src/worker.js
@@ -10,3 +10,42 @@ class MyTranslationPipeline {
     return this.instance;
   }
 }
+
+// Listen for messages from the main thread
+self.addEventListener('message', async (event) => {
+    // Retrieve the translation pipeline. When called for the first time,
+    // this will load the pipeline and save it for future use.
+    const translator = await MyTranslationPipeline.getInstance(x => {
+        // We also add a progress callback to the pipeline so that we can
+        // track model loading.
+        self.postMessage(x);
+    });
+  
+    // Capture partial output as it streams from the pipeline
+    const streamer = new TextStreamer(translator.tokenizer, {
+        skip_prompt: true,
+        skip_special_tokens: true,
+        callback_function: function (text) {
+            self.postMessage({
+                status: 'update',
+                output: text
+            });
+        }
+    });
+  
+    // Actually perform the translation
+    const output = await translator(event.data.text, {
+        tgt_lang: event.data.tgt_lang,
+        src_lang: event.data.src_lang,
+  
+        // Allows for partial output to be captured
+        streamer,
+    });
+  
+    // Send the output back to the main thread
+    self.postMessage({
+        status: 'complete',
+        output,
+    });
+});
+
